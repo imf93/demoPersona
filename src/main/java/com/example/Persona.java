e083a4178bf66b53ae09cc63a564dc82bc613a99
imf93 <imerinofajardo@gmail.com>
2016-07-12 10:33:06 +0200

    Hemos añadido para comparar edades mayor, o buscar personas con un nombre determinado.

--- src/main/java/com/example/Persona.java
+++ src/main/java/com/example/Persona.java
@@ -15,6 +15,9 @@ public class Persona {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nombre;
+    private String apellido;
+    private Integer edad;
+
 
     public Long getId() {
         return id;
@@ -32,11 +35,56 @@ public class Persona {
         this.nombre = nombre;
     }
 
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
     @Override
     public String toString() {
         return "Persona{" +
                 "id=" + id +
                 ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", edad=" + edad +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Persona persona = (Persona) o;
+
+        if (getId() != null ? !getId().equals(persona.getId()) : persona.getId() != null) return false;
+        if (getNombre() != null ? !getNombre().equals(persona.getNombre()) : persona.getNombre() != null) return false;
+        if (getApellido() != null ? !getApellido().equals(persona.getApellido()) : persona.getApellido() != null)
+            return false;
+        return getEdad() != null ? getEdad().equals(persona.getEdad()) : persona.getEdad() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getNombre() != null ? getNombre().hashCode() : 0);
+        result = 31 * result + (getApellido() != null ? getApellido().hashCode() : 0);
+        result = 31 * result + (getEdad() != null ? getEdad().hashCode() : 0);
+        return result;
+    }
 }
+
+
+
--- src/main/java/com/example/PersonaService.java
+++ src/main/java/com/example/PersonaService.java
@@ -15,13 +15,48 @@ public class PersonaService {
     public void testPersonas(){
         Persona persona = new Persona();
         persona.setNombre("Ivan");
+        persona.setEdad(23);
         personaRepository.save(persona);
 
         Persona persona2 = new Persona();
         persona2.setNombre("Dimple");
         personaRepository.save(persona2);
+        persona2.setEdad(25);
+        Persona persona3 = new Persona();
+
+        persona3.setNombre("Noelia");
+        persona3.setApellido("Villa");
+        persona3.setEdad(25);
+        personaRepository.save(persona3);
+
+        Persona persona4 = new Persona();
+        persona4.setNombre("Noelia");
+        persona4.setApellido("Hernan");
+
+
+        persona4.setEdad(25);
+        personaRepository.save(persona4);
+
+
+        Persona persona5 = new Persona();
+        persona5.setNombre("Ricard");
+        persona5.setApellido("Hernandez");
+        persona5.setEdad(22);
+        personaRepository.save(persona5);
 
         System.out.println(personaRepository.findOne(2L));
+
+        System.out.println(personaRepository.findByNombre("Noelia"));
+
+        System.out.println("Mostrar todas las personas cuya edad es>=22");
+        System.out.println(personaRepository.findByEdadGreaterThanEqual(23));
+
+        System.out.println("Mostramos todas las personas que se llamen Noelia Villa");
+        System.out.println(personaRepository.findByNombreAndApellido("Noelia", "Villa"));
+
+        System.out.println("Mostramos todas las personas que se llamen Noelia o bien el apellido sea Villa");
+        System.out.println(personaRepository.findByNombreOrApellido("Noelia", "Villa"));
+
 
 
 
